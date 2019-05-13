0e65ab0c3be29caa50b54d05a0acbdd354fffca0
gabriel schiavo <gabriel@lewagon.org>
2019-05-13 16:42:31 -0300

    add test for tdd

---
+++ 04-list-filtering/spec/04-list-filtering_spec.rb
@@ -0,0 +1,3 @@
+Test.assert_equals(filter_list([1,2,'a','b']),[1,2])
+Test.assert_equals(filter_list([1,'a','b',0,15]),[1,0,15])
+Test.assert_equals(filter_list([1,2,'aasf','1','123',123]),[1,2,123])
