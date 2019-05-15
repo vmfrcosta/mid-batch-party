fd740e3347e1e54881e0693b8fc53255acd06307
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 14:33:39 -0300

    update specs

--- 04-list-filtering/spec/04-list-filtering_spec.rb
+++ 04-list-filtering/spec/04-list-filtering_spec.rb
@@ -3,10 +3,10 @@ require 'list_filtering'
 describe "List Filter" do
 	it "Should list just 1 Number" do
 		# Code structure
-		Test.assert_equals(filter_list([1,2,'a','b']),[1,2])
+		expect(filter_list([1,2,'a','b'])).to eq([1,2])
 	end
 
 	it "Should list just 3 Numbers" do
-		Test.assert_equals(filter_list([1,'a','b',0,15]),[1,0,15])
+		expect(filter_list([1,'a','b',0,15])).to eq([1,0,15])
 	end
 end
