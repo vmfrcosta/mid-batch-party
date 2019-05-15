e7018e0c71308ac03349621946f1a0c13850ee60
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 12:21:59 -0300

    update rakefile and tdd

--- 02-sum-positive/spec/02-sum-positive_spec.rb
+++ 02-sum-positive/spec/02-sum-positive_spec.rb
@@ -1,5 +1,11 @@
+require "sum_positive"
+
 describe "positive_sum" do
   it "works for some examples" do
+    # expect(positive_sum([1,2,3,4,5])).to eq(15)
+    # expect(positive_sum([1,-2,3,4,5])).to eq(13)
+    # expect(positive_sum([-1,2,3,4,-5])).to eq(9)
+    
     Test.assert_equals positive_sum([1,2,3,4,5]), 15
     Test.assert_equals positive_sum([1,-2,3,4,5]), 13
     Test.assert_equals positive_sum([-1,2,3,4,-5]), 9
