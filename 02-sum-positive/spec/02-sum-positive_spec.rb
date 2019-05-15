6cd680df3ff3fd0c96bf22388c69dae551a64a96
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 12:25:59 -0300

    update second challenge

--- 02-sum-positive/spec/02-sum-positive_spec.rb
+++ 02-sum-positive/spec/02-sum-positive_spec.rb
@@ -2,20 +2,16 @@ require "sum_positive"
 
 describe "positive_sum" do
   it "works for some examples" do
-    # expect(positive_sum([1,2,3,4,5])).to eq(15)
-    # expect(positive_sum([1,-2,3,4,5])).to eq(13)
-    # expect(positive_sum([-1,2,3,4,-5])).to eq(9)
-    
-    Test.assert_equals positive_sum([1,2,3,4,5]), 15
-    Test.assert_equals positive_sum([1,-2,3,4,5]), 13
-    Test.assert_equals positive_sum([-1,2,3,4,-5]), 9
+    expect(positive_sum([1,2,3,4,5])).to eq(15)
+    expect(positive_sum([1,-2,3,4,5])).to eq(13)
+    expect(positive_sum([-1,2,3,4,-5])).to eq(9)
   end
 
   it "returns 0 when array is empty" do
-    Test.assert_equals positive_sum([]), 0
+    expect(positive_sum([])).to eq(0) 
   end
 
   it "returns 0 when all elements are negative" do
-    Test.assert_equals positive_sum([-1,-2,-3,-4,-5]), 0
+    expect(positive_sum([-1,-2,-3,-4,-5])).to eq(0)
   end
 end
