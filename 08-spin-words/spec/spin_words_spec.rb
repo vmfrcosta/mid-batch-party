d5499e70095d7cccd15b7b81885b183cc7f2a20a
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 18:08:29 -0300

    finish 8th challenge

--- 08-spin-words/spec/spin_words_spec.rb
+++ 08-spin-words/spec/spin_words_spec.rb
@@ -1,1 +1,15 @@
 require 'spin_words'
+
+describe "Should spin words for" do
+	it "'Hey fellow warriors' phrase" do
+		expect(spinWords("Hey fellow warriors")).to eq("Hey wollef sroirraw")
+	end
+
+	it "'This is a test' phrase" do
+		expect(spinWords("This is a test")).to eq("This is a test")
+	end
+
+	it "'This is another test' phrase" do
+		expect(spinWords("This is another test")).to eq("This is rehtona test")
+	end
+end
