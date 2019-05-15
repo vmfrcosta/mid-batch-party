ea658db0c51df4def29d00260063be0637d4f563
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 16:18:27 -0300

    add sixth test for challenge

--- 06-mumbling/spec/06-mumbling_spec.rb
+++ 06-mumbling/spec/06-mumbling_spec.rb
@@ -1,1 +1,16 @@
 require 'mumbling'
+
+describe "Should return" do
+	it "Sould return the certain characters number" do 
+		expect(accum("abcd")).to eq("A-Bb-Ccc-Dddd")
+	end
+
+	it "Sould return the certain characters number" do 
+		expect(accum("RqaEzty")).to eq("R-Qq-Aaa-Eeee-Zzzzz-Yyyyyy")
+	end
+
+	it "Sould return the certain characters number" do 
+		expect(accum("cwAt")).to eq("C-Ww-Aaa-Tttt")
+	end
+
+end
