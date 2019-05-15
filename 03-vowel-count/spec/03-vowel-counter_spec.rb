5f4b884c281c0d2c3b2e20e27751c0b6bbf87a52
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 13:48:36 -0300

    update fourth challenge

--- 03-vowel-count/spec/03-vowel-counter_spec.rb
+++ 03-vowel-count/spec/03-vowel-counter_spec.rb
@@ -1,11 +1,11 @@
 require 'vowel_count'
 
 describe "getCount" do
-  it "Should return vowel total count number" do
+  it "Should return vowel count for 'abracadabra'" do
     expect(getCount("abracadabra")).to eq(5)
   end
 
-	it "Should return vowel total count number" do
+	it "Should return vowel count for 'cat'" do
     expect(getCount("cat")).to eq(1)
   end
 end
--- 04-list-filtering/spec/04-list-filtering_spec.rb
+++ 04-list-filtering/spec/04-list-filtering_spec.rb
@@ -1,3 +1,12 @@
-Test.assert_equals(filter_list([1,2,'a','b']),[1,2])
-Test.assert_equals(filter_list([1,'a','b',0,15]),[1,0,15])
-Test.assert_equals(filter_list([1,2,'aasf','1','123',123]),[1,2,123])
+require 'list_filtering'
+
+describe "" do
+	it "Should list just 1 Number" do
+		# Code structure
+		Test.assert_equals(filter_list([1,2,'a','b']),[1,2])
+	end
+
+	it "Should list just 3 Numbers" do
+		Test.assert_equals(filter_list([1,'a','b',0,15]),[1,0,15])
+	end
+end
