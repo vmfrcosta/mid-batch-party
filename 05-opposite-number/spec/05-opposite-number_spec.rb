3e132cfe0594f267146deea3d1f3d991d4df3761
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 14:35:10 -0300

    add more TDD

--- 05-opposite-number/spec/05-opposite-number_spec.rb
+++ 05-opposite-number/spec/05-opposite-number_spec.rb
@@ -1,1 +1,7 @@
-Test.assert_equals(opposite(1), -1, msg = nil)
+require 'opposite_number'
+
+describe "Opposite number" do
+	it "should return a negative number" do
+		expect(opposite(1)).to eq(-1)
+	end
+end
