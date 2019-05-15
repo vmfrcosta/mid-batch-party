846df7b4f5f87434a2857ebbaf100de77c2fc331
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 11:37:04 -0300

    fix test script

---
+++ 01-even-or-odd/spec/even_or_odd_spec.rb
@@ -0,0 +1,8 @@
+require 'even_or_odd'
+
+describe "Solution" do
+  it "should test for something" do
+    # Test.assert_equals("actual", "expected", "This is just an example of how you can write your own TDD tests")
+    
+  end
+end
