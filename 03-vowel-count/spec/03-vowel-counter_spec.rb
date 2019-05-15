625d5bd17125645065dc65519697691dbe770f1c
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 12:35:49 -0300

    add expected tests results

--- 03-vowel-count/spec/03-vowel-counter_spec.rb
+++ 03-vowel-count/spec/03-vowel-counter_spec.rb
@@ -1,2 +1,11 @@
-Test.assert_equals(getCount("abracadabra"), 5)
-Test.assert_equals(getCount("cat"), 1)
+require 'vowel_count'
+
+describe "getCount" do
+  it "Should return vowel total count number" do
+    expect(getCount("abracadabra")).to eq(5)
+  end
+
+	it "Should return vowel total count number" do
+    expect(getCount("cat")).to eq(1)
+  end
+end
