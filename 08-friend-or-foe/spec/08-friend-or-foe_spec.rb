67cafa53bb43308eb9a7ff848cefc24cee3300b6
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 15:52:21 -0300

    update eight challenge

--- 08-friend-or-foe/spec/08-friend-or-foe_spec.rb
+++ 08-friend-or-foe/spec/08-friend-or-foe_spec.rb
@@ -2,10 +2,18 @@ require 'friend_or_foe'
 
 describe "Friends" do
 	it "should return all friends" do
-		expected(friend(["Ryan", "Kieran", "Mark"])).to eq(["Rayan", "Mark"))
+		expect(friend(["Ryan", "Kieran", "Mark"])).to eq(["Rayan", "Mark"])
+	end
+
+	it "should return some friends" do
+		expect(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"])).to eq(["Jimm", "Cari", "aret"])
+	end
+
+	it "should return a specific friend" do
+		expect(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])).to eq(["Ryan"])
+	end
+
+	it "should pass this test" do
+		expect(friend(["Love", "Your", "Face", "1"])).to eq(["Love", "Your", "Face"])
 	end
 end
-Test.assert_equals(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"])
-Test.assert_equals(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"])
-Test.assert_equals(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"])
-Test.assert_equals(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"])
