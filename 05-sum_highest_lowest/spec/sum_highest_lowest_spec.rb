632153d83a85fa1b1126828edc63b0c943ee0904
gabriel schiavo <gabriel@lewagon.org>
2019-05-15 17:42:58 -0300

    add spec script

--- 05-sum_highest_lowest/spec/sum_highest_lowest_spec.rb
+++ 05-sum_highest_lowest/spec/sum_highest_lowest_spec.rb
@@ -1,4 +1,11 @@
 require 'sum_highest_lowest'
 
 describe "Should return the right sum" do
+	it "for this array elements:" do
+		expect(sum([ 6, 2, 1, 8, 10 ]).to eq(16)
+	end
+
+	it "for this array elements:" do
+		expect(sum([ 1, 1, 11, 2, 3 ])).to eq(6)
+	end
 end
