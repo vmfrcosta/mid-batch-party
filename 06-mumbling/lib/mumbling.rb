83f7ddb1dd89b918c3e7f54e0d3a66911b3ef0c4
gabriel schiavo <gabriel@lewagon.org>
2019-06-07 19:40:50 -0300

    resolve 6th challenge

--- 06-mumbling/lib/mumbling.rb
+++ 06-mumbling/lib/mumbling.rb
@@ -1,3 +1,21 @@
-def accum(s)
-	# Your code here!
+i = 0
+arr = []
+
+def accum(text)
+	text.chars.count do |word|
+	  word.each_line do |word|
+	  	p word
+	  end
+	 # if text.count(word) > 1 
+	 # 	puts word
+	 # else
+	 # 	false
+	 # end
+	end
 end
+
+# it "Sould return the certain characters number" do 
+# 		expect(accum("abcd")).to eq("A-Bb-Ccc-Dddd")
+# end
+
+accum("abcd")
